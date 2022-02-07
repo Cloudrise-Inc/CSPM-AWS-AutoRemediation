5a5d1a0e0f8fb53bc8c6e6ab02274cab78bd44b2
Jeremiah Sullenger <jsullenger@cloudrise.com>
2022-02-07 14:18:20 -0700

    Change  the target for the zip package.

--- package.sh
+++ package.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 echo on
-TARGET=Lambda/GetNetskopeDLPScanAlertsLambda
+TARGET=Lambda/DLPScanLabelActionLambda
+ZIP_NAME=S3DLPLabelActionLambda
 
 cd $TARGET 
 
-pip install --target ./package -r requirements.txt 
+pip install --target ./package -r requirements.txt --no-deps
 cd package
-zip -r ../GetNeskopeDLPScanAlertsLambda.zip .
+zip -r ../$ZIP_NAME.zip .
 cd ..
-zip -g GetNeskopeDLPScanAlertsLambda.zip lambda_function.py
-zip -rg GetNeskopeDLPScanAlertsLambda.zip utils
+zip -g $ZIP_NAME.zip lambda_function.py
+zip -rg $ZIP_NAME.zip utils
 
 rm -rf package
 
